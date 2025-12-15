4118a3455f4d6de15adeb2d5ecb3984bb2d3a7ac
Nashit Hamza <nashit@Nashits-MacBook-Air.local>
2025-12-15 19:52:38 +0530

    design controllers

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,12 +1,56 @@
 const Book=require('../models/Book');
 
 const getAllBooks=async(req,res)=>{ 
+    try{
+        const findBooks=await Book.find({});
+        if(findBooks.length>0){
+            res.status(200).json({
+                success:true,
+                message:"List Sent successfully",
+                data:findBooks
+            });
+        }else{
+            res.status(404).json({
+                success:false,
+                message:"Unable to fetch books,no books available"
+            })
+        }
+
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            message:"Something went wrong"
+        })
+    }
 
 
 
 };
 
 const getBookById=async(req,res)=>{ 
+    try{
+        const bookId=req.params.id;
+        const singleBook=await Book.findById(bookId); 
+
+        if(!singleBook){
+            return res.json({
+                success:false,
+                message:`Book with id ${bookId} does not exist`, 
+
+            })
+        }
+        res.json( {
+            success:true, 
+            data:singleBook
+        })
+
+    }catch(e){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
 
     
 }; 
@@ -29,12 +73,54 @@ const createBook=async(req,res)=>{
 }; 
 
 const updateBook=async(req,res)=>{
+    try{
+    const updatedData=req.body; 
+    const Bookid=req.params.id;
+    const updatedBook= await Book.findByIdAndUpdate(
+    bookId,updateBook,
+    {new:true,}
+    );
+       if (!updatedBook) {
+            return res.status(404).json({
+                success: false,
+                message: "Book not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: updatedBook
+        });
+    }catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
 
 };
-
 const deleteBook=async(req,res)=>{
+    try{
+    const bookId=req.params.id; 
+    const bookToBeDeleted=await Book.findByIdAndDelete(bookId);
+    if(!bookToBeDeleted){
+        res.status(400).json({
+            success:false, 
+            message:"Unable to delete Book, Book does not exist"
+        })
+    }
+    }catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
 
 };
+
+const getBookbyQuery=async(req,res)=>{
+    // to be implmented later
+}
 
 module.exports={getAllBooks,
     getBookById,
